2a8081b9c11ae8d7a79e1ca3534288c6114c565c
Carl M. Kobel <kobel@pm.me>
2021-03-31 10:53:12 +0200

    amend

--- scripts/bash_extensions.sh
+++ scripts/bash_extensions.sh
@@ -8,15 +8,24 @@
 # Neat aliases
 alias survey='watch "sensors; nvidia-smi"'
 alias start_rampart='{ $(sleep 5; firefox localhost:3000)  & }; conda activate artic-rampart && rampart --protocol ~/repos/artic-ncov2019/rampart/ --clearAnnotated --basecalledPath'
-alias citament='git add -u && git commit -m "amend"; git pull && git push && echo OK'
+alias citament='git add -u && git commit -m "amend" &&  git pull && git push && echo OK'
+
 
 
 #alias pappenheim='cd ~/pappenheim && snakemake --profile default --config'
+
+
 start_pappenheim () {
+
+	example="\n\texample:\n\t start_pappenheim path/to/my_samplesheet.xlsx path/to/my_rundir\n"
+
  if [ -z "$1" ]; then
-     echo "The samplesheet argument is empty. Please specify a samplesheet."
+     echo "Input error: The samplesheet argument is empty. Please specify a samplesheet."
+     echo -e $example
+     
  elif [ -z "$2" ]; then
-     echo "The rundir argument is empty. Please specify a rundir."
+     echo "Input error: The rundir argument is empty. Please specify a rundir."
+     echo -e $example
  else
 
      #clear
