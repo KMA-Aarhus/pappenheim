218e0969a4e3c98fd3cdb87f7271494a8b44f290
(ontseqa) Carl M. Kobel <kobel@pm.me>
2021-04-20 14:14:35 +0200

    updated unhold script

--- workstation_setup/minknow_hold.sh
+++ workstation_setup/minknow_hold.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Mark all MinKNOW related packages as hold
+sudo apt-mark hold minion-nc
 sudo apt-mark hold minknow-core-minion-nc
+sudo apt-mark hold minknow-nc
 sudo apt-mark hold ont-bream4-minion
 sudo apt-mark hold ont-configuration-customer-minion
 sudo apt-mark hold ont-kingfisher-ui-minion
 sudo apt-mark hold ont-vbz-hdf-plugin
-sudo apt-mark hold minknow-nc
-
--- workstation_setup/minknow_unhold.sh
+++ workstation_setup/minknow_unhold.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-
-# Mark all MinKNOW related packages as auto
+#Mark all MinKNOW related packages as auto
+sudo apt-mark unhold minion-nc
 sudo apt-mark unhold minknow-core-minion-nc
+sudo apt-mark unhold minknow-nc
 sudo apt-mark unhold ont-bream4-minion
 sudo apt-mark unhold ont-configuration-customer-minion
 sudo apt-mark unhold ont-kingfisher-ui-minion
 sudo apt-mark unhold ont-vbz-hdf-plugin
-sudo apt-mark unhold minknow-nc
 
-
+sudo apt-mark auto minion-nc
+sudo apt-mark auto minknow-core-minion-nc
+sudo apt-mark auto minknow-nc
+sudo apt-mark auto ont-bream4-minion
+sudo apt-mark auto ont-configuration-customer-minion
+sudo apt-mark auto ont-kingfisher-ui-minion
+sudo apt-mark auto ont-vbz-hdf-plugin
 
